b1714ca476bb6b5294ea2310a27daa4208520dad
DudaRh16 <eduardarhayanefelix2009@gmail.com>
2025-09-05 13:59:18 +0000

    Atividade de lista "Salva" 9/5/2025

---
+++ Listas.py
@@ -0,0 +1,43 @@
+#Questão 1°
+'''frutas = ["morango", "maça", "kiwi", "banana", "laranja"]
+print(frutas[0])
+print(frutas[4])
+print(frutas)'''
+
+#Questão 2°
+'''numeros = [1, 2, 3, 4, 5]
+#a)
+numeros.append(6)
+#b)
+numeros.insert(0, 0)
+#c)
+numeros.remove(3)
+#d)
+print(numeros)'''
+
+#Questão 3°
+'''lista1 = [1, 2, 3]
+lista2 = [4, 5, 6]
+
+juntar = lista1 + lista2
+print(juntar)
+
+if 5 in juntar:
+    print("O número 5 está na lista")
+
+else:
+    print("O número 5 não está na lista")'''
+
+#Questão 4°
+'''letras = ['b', 'd', 'a', 'c', 'e']
+#a)
+letras.sort()
+print(letras)
+#b)
+letras.reverse()
+print(letras)
+#c)
+print(letras.count("a"))
+#d) 
+letras.pop()
+print(letras) '''
